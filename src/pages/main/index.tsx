1ba50ff02d1226e6ffb7eded83f572b16cb5a670
etoyadadaya <vanyasoup2014@cyber.sibsutis.ru>
2022-11-18 15:27:59 +0700

    Main page draft

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -5,9 +5,98 @@ import styles from "./styles.scss";
 
 const Main: FC = () => {
     return (
-        <div>
+        <>
+          <div className={styles.wrapper}>
             <Header/>
-        </div>
+            <div className={styles.filmsCoverRoot}>
+              <div className={styles.filmsCoverRow}>
+                <img className={styles.filmsCoverPoster} src={"https://avatars.mds.yandex.net/get-ott/239697/2a00000184332bae239ddba9f23b30436858/640x360"}/>
+                <img className={styles.filmsCoverPoster} src={"https://avatars.mds.yandex.net/get-ott/224348/2a00000181251bcfd826ce043197ba1651f5/640x360"}/>
+                <img className={styles.filmsCoverPoster} src={"https://avatars.mds.yandex.net/get-ott/1672343/2a0000017a456c3d040751b5020a183cbf14/640x360"}/>
+                <img className={styles.filmsCoverPoster} src={"https://avatars.mds.yandex.net/get-ott/2419418/2a0000016e3c80a98752efb4b023bf1aed80/640x360"}/>
+                <img className={styles.filmsCoverPoster} src={"https://avatars.mds.yandex.net/get-ott/2419418/2a000001847baa92d89b9025c2862d56c873/640x360"}/>
+                <img className={styles.filmsCoverPoster} src={"https://avatars.mds.yandex.net/get-ott/2385704/2a00000183f93c2bea9235cc9ce78b1752f9/640x360"}/>
+                <img className={styles.filmsCoverPoster} src={"https://avatars.mds.yandex.net/get-ott/2419418/2a000001845e4c4132e5575241a1ef0a6753/640x360"}/>
+              </div>
+              <div className={styles.filmsCoverRow}>
+                <img className={styles.filmsCoverPoster} src={"https://avatars.mds.yandex.net/get-ott/1672343/2a00000173bf955fb80e6a61b438f25e541b/640x360"}/>
+                <img className={styles.filmsCoverPoster} src={"https://avatars.mds.yandex.net/get-ott/239697/2a0000017e4095096a12d97324a7607c8b08/640x360"}/>
+                <img className={styles.filmsCoverPoster} src={"https://avatars.mds.yandex.net/get-ott/1652588/2a0000018383a59e48417d01be76ba656a4a/640x360"}/>
+                <img className={styles.filmsCoverPoster} src={"https://avatars.mds.yandex.net/get-ott/223007/2a0000018486db0a2970de2597ea7a27398b/640x360"}/>
+                <img className={styles.filmsCoverPoster} src={"https://avatars.mds.yandex.net/get-ott/223007/2a00000183cd0859ceb20d7b6b37f293d89d/640x360"}/>
+                <img className={styles.filmsCoverPoster} src={"https://avatars.mds.yandex.net/get-ott/224348/2a00000176f1c743efe3d925e7d13134d267/640x360"}/>
+                <img className={styles.filmsCoverPoster} src={"https://avatars.mds.yandex.net/get-ott/239697/2a00000184332bae239ddba9f23b30436858/640x360"}/>
+              </div>
+              <div className={styles.filmsCoverRow}>
+                <img className={styles.filmsCoverPoster} src={"https://avatars.mds.yandex.net/get-ott/1672343/2a00000181dfbe4c4da4e312c77841a751d2/640x360"}/>
+                <img className={styles.filmsCoverPoster} src={"https://avatars.mds.yandex.net/get-ott/1672343/2a00000176467320ddc898f1d435637e8297/640x360"}/>
+                <img className={styles.filmsCoverPoster} src={"https://avatars.mds.yandex.net/get-ott/1531675/2a0000018444767d513c2b986ee4bf434194/640x360"}/>
+                <img className={styles.filmsCoverPoster} src={"https://avatars.mds.yandex.net/get-ott/212840/2a00000184326b91354fc01aaca4faabdbbe/640x360"}/>
+                <img className={styles.filmsCoverPoster} src={"https://avatars.mds.yandex.net/get-ott/224348/2a00000183a4782a1f0d3dea2019977aa19e/640x360"}/>
+                <img className={styles.filmsCoverPoster} src={"https://avatars.mds.yandex.net/get-ott/1534341/2a0000018414b66f18b8944373a661c08707/640x360"}/>
+                <img className={styles.filmsCoverPoster} src={"https://avatars.mds.yandex.net/get-ott/1672343/2a00000181dfbc0f0f58e32d30ffb3858832/640x360"}/>
+              </div>
+              <div className={styles.filmsCoverRow}>
+                <img className={styles.filmsCoverPoster} src={"https://avatars.mds.yandex.net/get-ott/200035/2a0000017f31d7108fa2c5aa4d89f3b9aa03/640x360"}/>
+                <img className={styles.filmsCoverPoster} src={"https://avatars.mds.yandex.net/get-ott/239697/2a000001838ebb91b0f7b7f44874bf5ae320/640x360"}/>
+                <img className={styles.filmsCoverPoster} src={"https://avatars.mds.yandex.net/get-ott/1534341/2a00000183c6cf70104ce06a3a2fa6b97f15/640x360"}/>
+                <img className={styles.filmsCoverPoster} src={"https://avatars.mds.yandex.net/get-ott/239697/2a0000017f4e3669c5eafef8a995bee11be4/640x360"}/>
+                <img className={styles.filmsCoverPoster} src={"https://avatars.mds.yandex.net/get-ott/2439731/2a00000180b9d0d1315574eb49664ed12fd5/640x360"}/>
+                <img className={styles.filmsCoverPoster} src={"https://avatars.mds.yandex.net/get-ott/224348/2a0000017618621f4cc67ee1009067626bb8/640x360"}/>
+                <img className={styles.filmsCoverPoster} src={"https://avatars.mds.yandex.net/get-ott/374297/2a000001840ad49f745c1e2b2a1a438499c0/640x360"}/>
+              </div>
+              <div className={styles.filmsCoverRow}>
+                <img className={styles.filmsCoverPoster} src={"https://avatars.mds.yandex.net/get-ott/212840/2a0000017c98a9ea762f93d5140d72322f7f/640x360"}/>
+                <img className={styles.filmsCoverPoster} src={"https://avatars.mds.yandex.net/get-ott/212840/2a0000016f13253813d444ec024b0499b787/640x360"}/>
+                <img className={styles.filmsCoverPoster} src={"https://avatars.mds.yandex.net/get-ott/236744/2a0000018047a6cf1d99b3d6c67243edb539/640x360"}/>
+                <img className={styles.filmsCoverPoster} src={"https://avatars.mds.yandex.net/get-ott/1534341/2a00000183ac2e71ea789b1fedbf66cff7e2/640x360"}/>
+                <img className={styles.filmsCoverPoster} src={"https://avatars.mds.yandex.net/get-ott/1652588/2a00000180b9ee76fdf0b5b09f642d451ff8/640x360"}/>
+                <img className={styles.filmsCoverPoster} src={"https://avatars.mds.yandex.net/get-ott/1531675/2a00000170cd8de3075f04b119ded06a7e50/640x360"}/>
+                <img className={styles.filmsCoverPoster} src={"https://avatars.mds.yandex.net/get-ott/2385704/2a00000180479b45bc662f63629a474224c5/640x360"}/>
+              </div>
+              <div className={styles.filmsCoverRow}>
+                <img className={styles.filmsCoverPoster} src={"https://avatars.mds.yandex.net/get-ott/224348/2a00000184768c3b9cfc0c5aacb4642b8c3d/640x360"}/>
+                <img className={styles.filmsCoverPoster} src={"https://avatars.mds.yandex.net/get-ott/2419418/2a0000017603e606a262312e4832ec242fb9/640x360"}/>
+                <img className={styles.filmsCoverPoster} src={"https://avatars.mds.yandex.net/get-ott/223007/2a00000173c31dc44be9e5760ea4aae0a9b8/640x360"}/>
+                <img className={styles.filmsCoverPoster} src={"https://avatars.mds.yandex.net/get-ott/2419418/2a0000016e3c6ad8f2df6df5e5857e61101b/640x360"}/>
+                <img className={styles.filmsCoverPoster} src={"https://avatars.mds.yandex.net/get-ott/2439731/2a0000017062fe735535e69953f2549b9640/640x360"}/>
+                <img className={styles.filmsCoverPoster} src={"https://avatars.mds.yandex.net/get-ott/2419418/2a0000016e3c7152e1ec1534220469adf4b8/640x360"}/>
+                <img className={styles.filmsCoverPoster} src={"https://avatars.mds.yandex.net/get-ott/1672343/2a000001725c9169be8f63f8f05b129d2b36/640x360"}/>
+              </div>
+            </div>
+            <div className={styles.onBoardRoot}>
+              <div className={styles.onBoardContent}>
+                <div className={styles.onBoardHeader}>
+                  <svg className={styles.logo} fill="none" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" data-tid="ba192876">
+                    <g>
+                      <path d="M15.003 35.352h5.151V5.766h-7.492c-7.535 0-11.494 3.874-11.494 9.578 0 4.555 2.171 7.237 6.045 10.004L.487 35.352h5.577l7.492-11.196-2.597-1.745c-3.15-2.129-4.683-3.79-4.683-7.365 0-3.15 2.214-5.279 6.428-5.279h2.3v25.585Zm99.317.426c2.341 0 4.002-.426 5.236-1.32v-4.13c-1.277.895-2.809 1.448-4.938 1.448-3.618 0-5.108-2.81-5.108-7.237 0-4.64 1.83-7.024 5.151-7.024 1.958 0 3.874.681 4.895 1.32v-4.3c-1.064-.596-2.937-1.022-5.449-1.022-6.47 0-9.834 4.64-9.834 11.154 0 7.152 3.278 11.11 10.047 11.11Zm-31.672-1.873v-4.13c-1.575 1.065-4.215 2.001-6.684 2.001-3.704 0-5.108-1.745-5.321-5.321H82.86v-2.682c0-7.45-3.278-10.26-8.344-10.26-6.173 0-9.11 4.726-9.11 11.196 0 7.45 3.66 11.069 10.131 11.069 3.236 0 5.62-.852 7.11-1.873ZM36.246 13.939v8.472h-6.769v-8.472h-5.066v21.413h5.066v-8.94h6.769v8.94h5.066V13.94h-5.066ZM64.172 31.35h-2.256V13.94H47.144v1.83c0 5.236-.34 12.005-2.128 15.58H43.44v8.898h4.683v-4.896H59.49v4.896h4.683V31.35Zm34.653 4.002h5.746l-8.13-11.536 7.151-9.877h-5.108l-7.152 9.877v-9.877h-5.066v21.413h5.066V24.837l7.493 10.515ZM74.431 17.515c2.511 0 3.278 2.086 3.278 4.768v.426h-7.067c.128-3.406 1.362-5.194 3.789-5.194ZM56.85 31.35h-7.152c1.405-3.235 1.788-9.067 1.788-12.77v-.64h5.364v13.41Z" fill="#fff"></path>
+                      <path d="M130.746 5.206c-2.287 3.32-2.287 8.055-2.287 17.525 0 9.471 0 14.206 2.287 17.526a12.3 12.3 0 0 0 2.649 2.794c3.147 2.412 7.636 2.412 16.615 2.412h59.864c8.979 0 13.469 0 16.616-2.412a12.344 12.344 0 0 0 2.649-2.794c2.286-3.32 2.286-8.055 2.286-17.526 0-9.47 0-14.206-2.286-17.525a12.344 12.344 0 0 0-2.649-2.794C223.343 0 218.853 0 209.874 0H150.01c-8.979 0-13.468 0-16.615 2.412a12.3 12.3 0 0 0-2.649 2.794Zm63.551 8.335c-4.227 0-7.303 2.58-7.934 7.285h-2.52V13.89h-4.412v17.532h4.412v-7.32h2.483c.482 4.95 3.633 7.669 7.971 7.669 4.745 0 8.044-3.242 8.044-9.132 0-5.856-3.299-9.098-8.044-9.098Zm0 14.953c-2.336 0-3.485-1.777-3.485-5.82 0-4.078 1.149-5.856 3.485-5.856 2.335 0 3.484 1.777 3.484 5.82 0 4.079-1.149 5.856-3.484 5.856Zm-52.696 2.928V13.89h14.532v17.532h-4.411V17.166h-5.709v14.256h-4.412Zm17.762 0v-3.59s.371.314 1.001.314c1.446 0 1.706-1.15 1.854-4.88l.371-9.376h12.641v17.532h-4.412V17.166h-4.485l-.26 6.274c-.222 5.821-1.186 8.33-4.967 8.33-1.038 0-1.743-.348-1.743-.348Zm59.261-.732c-1.075.732-2.521 1.08-4.56 1.08-5.894 0-8.748-3.24-8.748-9.096 0-5.333 2.928-9.133 8.563-9.133 2.187 0 3.818.35 4.745.837v3.52c-.89-.522-2.558-1.08-4.263-1.08-2.892 0-4.486 1.952-4.486 5.751 0 3.625 1.298 5.925 4.449 5.925 1.853 0 3.188-.453 4.3-1.185v3.381Z" fillRule="evenodd" fill="url(#YandexPlusLogo_gradient)"></path>
+                      <path d="M141.603 13.89v17.532h4.411V17.166h5.709v14.256h4.412V13.89h-14.532ZM159.365 27.832v3.59s.704.348 1.742.348c3.782 0 4.745-2.51 4.968-8.33l.259-6.274h4.486v14.256h4.411V13.89H162.59l-.371 9.376c-.148 3.73-.407 4.88-1.853 4.88-.63 0-1.001-.314-1.001-.314ZM194.298 13.541c-4.226 0-7.303 2.58-7.933 7.285h-2.521V13.89h-4.411v17.532h4.411v-7.32h2.484c.482 4.95 3.633 7.668 7.97 7.668 4.745 0 8.044-3.241 8.044-9.132 0-5.856-3.299-9.097-8.044-9.097Zm0 14.953c-2.335 0-3.485-1.778-3.485-5.82 0-4.079 1.15-5.857 3.485-5.857 2.336 0 3.485 1.778 3.485 5.821 0 4.078-1.149 5.856-3.485 5.856ZM214.066 31.77c2.039 0 3.484-.348 4.56-1.08v-3.381c-1.113.732-2.447 1.185-4.301 1.185-3.151 0-4.448-2.3-4.448-5.925 0-3.8 1.594-5.752 4.485-5.752 1.706 0 3.374.558 4.264 1.08v-3.52c-.927-.487-2.558-.836-4.746-.836-5.634 0-8.563 3.8-8.563 9.132 0 5.856 2.855 9.097 8.749 9.097Z" fill="#fff"></path>
+                    </g>
+                    <defs>
+                      <linearGradient id="YandexPlusLogo_gradient" x1="128.459" y1="45.593" x2="224.38" y2="-7.278" gradientUnits="userSpaceOnUse">
+                        <stop offset=".036" stopColor="#FF5C00"></stop>
+                        <stop offset=".208" stopColor="#EA507C"></stop>
+                        <stop offset=".391" stopColor="#B941EF"></stop>
+                        <stop offset=".651" stopColor="#5B61FF"></stop>
+                        <stop offset=".948" stopColor="#21BEE7"></stop>
+                      </linearGradient>
+                    </defs>
+                  </svg>
+                  <h1 className={styles.textRootH1}>Фильмы и сериалы по подписке Плюс Мульти</h1>
+                  <h2 className={styles.textRootH2}>Бесплатно до конца года</h2>
+                  <p className={styles.textRootP}>Отменяйте в любой момент, напишем за 3 дня до первого списания</p>
+                </div>
+              </div>
+              <div className={styles.buttonWrap}>
+                <button className={styles.button}>Попробовать бесплатно до конца года</button>
+              </div>
+              <div className={styles.roll}>
+                <p className={styles.rollText}>Подробнее о подписке</p>
+              </div>
+            </div>
+          </div>
+        </>
     );
 };
 
